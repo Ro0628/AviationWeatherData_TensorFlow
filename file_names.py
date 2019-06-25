0521b282e65e506d59e683da5ec8cb923f8d731b
Ro0628 <ronaldalajuan@gmail.com>
2019-06-25 00:17:41 -0500

    initial commit

---
+++ file_names.py
@@ -0,0 +1,10 @@
+
+
+#global file names
+file_orig = "/Users/ronaldajohnson/PycharmProjects/AviationWeatherForecasting/data/WeatherData.csv"
+file_cleaned = "/Users/ronaldajohnson/PycharmProjects/AviationWeatherForecasting/data/WeatherData_cleaned.csv"
+file_train_split = "/Users/ronaldajohnson/PycharmProjects/AviationWeatherForecasting/data/WeatherData_TrainingSet_Split.csv"
+file_test_split = "/Users/ronaldajohnson/PycharmProjects/AviationWeatherForecasting/data/WeatherData_TestingSet_Split.csv"
+file_train  = "/Users/ronaldajohnson/PycharmProjects/AviationWeatherForecasting/data/WeatherData_TrainingSet.csv"
+file_test = "/Users/ronaldajohnson/PycharmProjects/AviationWeatherForecasting/data/WeatherData_TestingSet.csv"
+
